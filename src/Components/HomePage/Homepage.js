9b35f7b90c4026cc0774c5a6c1c9a3158d0a2b9a
Harrison <Harrisontbusby@gmail.com>
2023-03-27 14:48:53 -0700

    Homepage Push

--- src/Components/HomePage/Homepage.js
+++ src/Components/HomePage/Homepage.js
@@ -16,10 +16,21 @@ export default function Homepage() {
                 </Col>
 
                 <Col>
-                    <h1 className=''>Looking for the perfect PC build? You’ve come
+                    <h1 className='bannerFont'>Looking for the perfect PC build? You’ve come
                         to the right place!</h1>
+                    <Row>
+                        <Col>
+                            <h1 className='bannerFont2'>Lets Get Started!</h1>
+                        </Col>
+                    </Row>
                 </Col>
             </Row>
+
+            <Container className='quizBg'>
+                <Row>
+                    <h1 className='quizFont1 mt-5'>What parts are you looking for?</h1>
+                </Row>
+            </Container>
 
         </body>
     )
