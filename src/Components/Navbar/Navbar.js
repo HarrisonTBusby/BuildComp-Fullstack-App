b7719c974de613fc37ea6edb7ce1ea2f6351bb2a
Harrison <Harrisontbusby@gmail.com>
2023-03-27 14:17:50 -0700

    finished Nav

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Navbar.css';
 import {Container, Navbar, Nav} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import {Link} from 'react-router-dom';
 import Logo from '../../Assets/BGLogoRB.png';
 export default function NavbarComponent() {
@@ -9,11 +10,11 @@ export default function NavbarComponent() {
     <Container>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-                <Nav.Link as={Link} to='/' className='fontColor'>Home</Nav.Link>
-                <Nav.Link as={Link} to='/BestBuilds' className='fontColor'>Best Builds</Nav.Link>
-                <Nav.Link as={Link} to='/Wishlist' className='fontColor'>Wishlist</Nav.Link>
-                <Nav.Link as={Link} to='/PreviousSelections' className='fontColor'>Previous Selections</Nav.Link>
+            <Nav className="mr-5">
+                <Nav.Link as={Link} to='/' className='fontColor mx-5'>Home</Nav.Link>
+                <Nav.Link as={Link} to='/BestBuilds' className='fontColor mx-5'>Best Builds</Nav.Link>
+                <Nav.Link as={Link} to='/Wishlist' className='fontColor mx-5'>Wishlist</Nav.Link>
+                <Nav.Link as={Link} to='/PreviousSelections' className='fontColor mx-5'>Previous Selections</Nav.Link>
             </Nav>
         </Navbar.Collapse>
         <Navbar.Brand as={Link} to='/' className='fontColor'><img className='logoSize' src={Logo}/>BuildComp</Navbar.Brand>
