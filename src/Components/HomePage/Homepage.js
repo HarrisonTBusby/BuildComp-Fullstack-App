419dbdd7e617dd0b733b14ff461fcb4e9863d09c
Harrison <Harrisontbusby@gmail.com>
2023-03-27 14:30:11 -0700

    Fonts

--- src/Components/HomePage/Homepage.js
+++ src/Components/HomePage/Homepage.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import { Container, Row, Col} from 'react-bootstrap'
+import { Container, Row, Col } from 'react-bootstrap'
 import NavbarComponent from '../Navbar/Navbar.js';
+import Banner from '../../Assets/GamingImage.jpg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Homepage.css';
 import { Link } from 'react-router-dom';
@@ -8,10 +9,15 @@ import { Link } from 'react-router-dom';
 export default function Homepage() {
     return (
         <body>
-           <NavbarComponent/>
-            <Row>
-                <Col className='col-12'>
-                    
+            <NavbarComponent />
+            <Row className='BannerImg'>
+                <Col>
+
+                </Col>
+
+                <Col>
+                    <h1 className=''>Looking for the perfect PC build? You’ve come
+                        to the right place!</h1>
                 </Col>
             </Row>
 
