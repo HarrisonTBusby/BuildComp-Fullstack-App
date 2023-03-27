b193b25d0e4badcbad153f6279965085c507729b
Harrison <Harrisontbusby@gmail.com>
2023-03-27 14:13:30 -0700

    Favicon Fix

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -10,13 +10,13 @@ export default function NavbarComponent() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-                <Nav.Link as={Link} to='/'>Home</Nav.Link>
-                <Nav.Link as={Link} to='/BestBuilds'>Best Builds</Nav.Link>
-                <Nav.Link as={Link} to='/Wishlist'>Wishlist</Nav.Link>
-                <Nav.Link as={Link} to='/PreviousSelections'>Previous Selections</Nav.Link>
+                <Nav.Link as={Link} to='/' className='fontColor'>Home</Nav.Link>
+                <Nav.Link as={Link} to='/BestBuilds' className='fontColor'>Best Builds</Nav.Link>
+                <Nav.Link as={Link} to='/Wishlist' className='fontColor'>Wishlist</Nav.Link>
+                <Nav.Link as={Link} to='/PreviousSelections' className='fontColor'>Previous Selections</Nav.Link>
             </Nav>
         </Navbar.Collapse>
-        <Navbar.Brand as={Link} to='/'><img className='logoSize' src={Logo}/>BuildComp</Navbar.Brand>
+        <Navbar.Brand as={Link} to='/' className='fontColor'><img className='logoSize' src={Logo}/>BuildComp</Navbar.Brand>
     </Container>
 </Navbar>
   )
