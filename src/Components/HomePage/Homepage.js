ed50c3a2a108c827dc3aa5e26c05ab4cdb8d5ae6
Harrison <Harrisontbusby@gmail.com>
2023-03-27 15:19:59 -0700

    Homepage complete besides footer

--- src/Components/HomePage/Homepage.js
+++ src/Components/HomePage/Homepage.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col, Dropdown, Button } from 'react-bootstrap'
 import NavbarComponent from '../Navbar/Navbar.js';
 import Banner from '../../Assets/GamingImage.jpg';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -27,8 +27,30 @@ export default function Homepage() {
             </Row>
 
             <Container className='quizBg'>
-                <Row>
-                    <h1 className='quizFont1 mt-5'>What parts are you looking for?</h1>
+                <Row className='center'>
+                    <h1 className='quizFont1 marginTop'>What parts are you looking for?</h1>
+                </Row>
+                <Row className='center marginTop'>
+                    <Dropdown className='center'>
+                        <Dropdown.Toggle className='dropdownSize'>
+                           PC Components
+                        </Dropdown.Toggle>
+
+                        <Dropdown.Menu className='ddButton'>
+                            <Dropdown.Item value='Gpu'>CPU</Dropdown.Item>
+                            <Dropdown.Item value='Cpu'>GPU</Dropdown.Item>
+                            <Dropdown.Item value='Motherboard'>Motherboard</Dropdown.Item>
+                            <Dropdown.Item value='Case'>Case</Dropdown.Item>
+                            <Dropdown.Item value='Fan'>Fans</Dropdown.Item>
+                            <Dropdown.Item value='Ram'>RAM</Dropdown.Item>
+                            <Dropdown.Item value='Power Supply'>Power Supply</Dropdown.Item>
+                            <Dropdown.Item value='Heat Sink'>Heat Sink</Dropdown.Item>
+                            <Dropdown.Item value='Hard Drives'>Hard Drives</Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
+                </Row>
+                <Row className='center marginTop'>
+                    <Button className='submitBtn'>Submit</Button>
                 </Row>
             </Container>
 
