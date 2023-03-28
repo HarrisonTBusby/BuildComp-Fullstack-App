9a4ead91be747e10484b9458cdc2a08f544663c0
Reed Goodwin <113049336+Soup-Mann@users.noreply.github.com>
2023-03-28 14:30:47 -0700

    wish

---
+++ src/Components/WishlistPage/WishlisltComp.js
@@ -0,0 +1,30 @@
+import React from 'react'
+// import './PreviousSelections.css';
+import { Link } from 'react-router-dom';
+import {Container, Row, Col,} from 'react-bootstrap'
+
+
+export default function wishlistRow() {
+    return (
+        <div>
+            <br></br>
+            <Row className='rowBack'>
+                <Col> pic
+                
+                </Col>
+
+                <Col> pic
+                
+                </Col>
+
+                <Col> pic
+                
+                </Col>
+
+                <Col> pic
+                
+                </Col>
+
+            </Row>
+        </div>
+    )}
