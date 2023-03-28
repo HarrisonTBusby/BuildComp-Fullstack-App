d6b6963b1e0c2f152e2c12bf4ae9ef5c8b971356
Harrison <Harrisontbusby@gmail.com>
2023-03-28 11:11:24 -0700

    Best builds

--- src/Components/BestBuildsPage/BestBuilds.js
+++ src/Components/BestBuildsPage/BestBuilds.js
@@ -1,9 +1,23 @@
 import React from 'react'
 import './BestBuilds.css'
+import NavbarComponent from '../Navbar/Navbar';
+import { Container, Row, Col, Dropdown, Form, Input } from 'react-bootstrap';
+import Footer from '../Footer/Footer';
 import { Link } from 'react-router-dom';
 
 export default function BestBuilds() {
-  return (
-    <div>BestBuilds</div>
-  )
+    return (
+        <body>
+            <NavbarComponent />
+            
+                <Row>
+                    <Col className='span filterBackground'>
+
+                    </Col>  
+                    <Col className='marginLeft'></Col>
+                </Row>
+           
+            <Footer />
+        </body>
+    )
 }
