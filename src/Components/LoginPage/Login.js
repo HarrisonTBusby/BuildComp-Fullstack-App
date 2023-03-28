e191672d4527c0c7ed94e264ec8266387a29374b
nauticuff <felipecarlos200x@gmail.com>
2023-03-28 15:14:07 -0700

    in progress login bootstrap conversion

--- src/Components/LoginPage/Login.js
+++ src/Components/LoginPage/Login.js
@@ -1,42 +1,50 @@
 import BuildCompLogo from '../../Assets/Logo.png'
 import './Login.css';
 import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const Login = () => {
     return (
-        <div className='login-wrapper'>
-            <div className="login-container">
-                <div className="left-login">
-                    <div className='left-login-top'>
-                        <img className="logo" src={BuildCompLogo}></img>
-                        <h1>BuildComp</h1>
-                    </div>
-                </div>
-                <div className="right-login">
-                    <h1>Welcome back</h1>
-                    <p>Please login to your account.</p>
-                    <label style={{display: 'block'}}>
-                        Username
-                        <div>
-                            <input type="text" name="username"></input>
-                        </div>
-                    </label>
-                    <label style={{display: 'block'}}>
-                        Password
-                        <div>
-                            <input type="password" name="password"></input>
-                        </div>
-                    </label>
-                    <button className='login-btn'>Login</button>
-                    <div className='divider'>
-                        <hr ></hr>
-                        <p className='or'>or</p>
-                    </div>
-                    <Link to='/'><button className='login-btn'>Continue as guest</button></Link>
-                    <p className='create-account'>Don't have an account <Link to='/SignUp'>Sign up</Link></p>
-                </div>
-            </div>
-        </div>
+        <Container className='login-wrapper d-flex justify-content-center align-items-center'>
+            <Container className="login-container position-relative">
+                <Container className="left-login">
+                    <Row className='left-login-top mt-4'>
+                        <Col className='d-flex justify-content-center align-items-center'>
+                            <img className="login-logo" src={BuildCompLogo} alt='BuildComp logo'></img>
+                            <h1>BuildComp</h1>
+                        </Col>
+                    </Row>
+                </Container>
+                <Container className="right-login">
+                    <Row className='justify-content-center'>
+                        <Col md='6'>
+                            <h2>Welcome back</h2>
+                            <p>Please login to your account.</p>
+                            <label className='w-100'>
+                                Username
+                                <div>
+                                    <input type="text" name="username"></input>
+                                </div>
+                            </label>
+                            <label className='w-100'>
+                                Password
+                                <div>
+                                    <input type="password" name="password"></input>
+                                </div>
+                            </label>
+                            <button className='login-btn'>Login</button>
+                            <div className='divider'>
+                                <hr ></hr>
+                                <p className='or'>or</p>
+                            </div>
+                            <Link to='/'><button className='login-btn'>Continue as guest</button></Link>
+                            <p className='create-account'>Don't have an account <Link to='/SignUp'>Sign up</Link></p>
+                        </Col>
+                    </Row>
+                </Container>
+            </Container>
+        </Container>
     );
 }
 
