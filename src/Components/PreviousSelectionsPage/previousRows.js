1aa6fb8bf8ba7026e039d371458f0d3621ca1e1b
Reed Goodwin <113049336+Soup-Mann@users.noreply.github.com>
2023-03-28 11:11:04 -0700

    startup

---
+++ src/Components/PreviousSelectionsPage/previousRows.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './PreviousSelections.css';
+import { Link } from 'react-router-dom';
+import {Container, Row, Col,} from 'react-bootstrap'
+
+export default function SelectionsRow() {
+    return (
+        <div>
+            <Row>
+                <Col></Col>
+
+
+            </Row>
+        </div>
+    )}
