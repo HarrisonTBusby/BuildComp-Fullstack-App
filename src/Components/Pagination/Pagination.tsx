d225fd2fd4b6df2f7baeafb4e8c70a5b3c9ad075
nauticuff <felipecarlos200x@gmail.com>
2023-04-12 13:33:49 -0700

    search by input and sort alphabetically added

--- src/Components/Pagination/Pagination.tsx
+++ src/Components/Pagination/Pagination.tsx
@@ -2,40 +2,46 @@ import React, { useEffect, useState } from "react";
 import { Button, Card } from "react-bootstrap";
 import Paginate from "react-paginate";
 import { GetRandomUserData } from "../../Services/DataService";
+import { PeopleAPIResponse, PeopleData } from "../../Services/DataService";
 
 const PaginationExample = () => {
-  const [data, setData] = useState([]);
+
+  const [sort, setSort] = useState('unsorted');
+  const [searchInput, setSearchInput] = useState('');
+  const [data, setData] = useState<PeopleData[]>([]);
+  const [sortedData, setSortedData] = useState<PeopleData[]>([]);
   const [currentPage, setCurrentPage] = useState<number>(0);
-
-  const ITEMS_PER_PAGE = 6;
   
-  let size = data.length;
-  const TOTAL_ITEMS = size;
-
-  const totalPages = Math.ceil(TOTAL_ITEMS / ITEMS_PER_PAGE);
-
   const handlePageChange = (selectedPage: { selected: number }) => {
     setCurrentPage(selectedPage.selected);
   };
 
-  const ItemList = () => {
+  const ITEMS_PER_PAGE = 6;
+  
+  let size = sortedData.length;
+  const TOTAL_ITEMS = size;
+
+  const totalPages = Math.ceil(TOTAL_ITEMS / ITEMS_PER_PAGE);
+  
+
+  const ItemList = ({sortedData}: any) => {
     const startIndex = currentPage * ITEMS_PER_PAGE;
     const endIndex = startIndex + ITEMS_PER_PAGE;
-    const itemsToDisplay = data.slice(startIndex, endIndex);
+    const itemsToDisplay = sortedData.slice(startIndex, endIndex);
 
     return (
       <div className="cards">
-        {itemsToDisplay.map((item: any, index) => (
+        {itemsToDisplay.map((item: any, index: number) => (
           <div key={index}>
             <Card style={{ width: '16rem', height: '100%'}}>
               <Card.Img variant="top" src={item.picture.medium} />
               <Card.Body>
-                <Card.Title>{item.name.first} {item.name.last}</Card.Title>
-                <Card.Text>
-                  <p>{item.cell}</p>
-                  <p>{item.email}</p>
-                  <h2>{item.location.country}</h2>
-                </Card.Text>
+                <Card.Title>{item.firstName} {item.lastName}</Card.Title>
+                <div>
+                  <div>{item.cell}</div>
+                  <div>{item.email}</div>
+                  <div>{item.location.country}</div>
+                </div>
                 <Button variant="primary">Go somewhere</Button>
               </Card.Body>
             </Card>
@@ -45,12 +51,11 @@ const PaginationExample = () => {
     );
   };
 
-  
-
   const getData = async () => {
+    
     let data = await GetRandomUserData();
-    setData(data.results);
-    // console.log(data.results)
+    setData(data.data);
+    setSortedData(data.data);
   }
 
   useEffect(() => {
@@ -58,10 +63,71 @@ const PaginationExample = () => {
     
   }, [])
 
+  const sortAlphabetically = () => {
+    const newData = [...sortedData].sort((a, b) => {
+      if (a.name.first < b.name.first) {
+        if(sort === 'unsorted'){
+          return -1;
+        }
+        if(sort === 'ascending'){
+          return 1;
+        }
+      }
+
+      if (a.name.first > b.name.first) {
+        if(sort === 'unsorted'){
+          return 1;
+        }
+        if(sort === 'ascending'){
+          return -1;
+        }
+      }
+      return 0;
+    });
+
+    setSortedData(newData);
+    if(sort === 'unsorted'){
+      setSort('ascending');
+    }
+    if(sort === 'ascending'){
+      setSort('descending');
+    }
+    if(sort === 'descending'){
+      setSortedData(data);
+      setSort('unsorted')
+    }
+  };
+
+  // const sortByPrice = (minPrice: number, maxPrice: number) => {
+  //   let filteredItems = sortedData.filter((value) => sortedData.value > 5);
+  //   filteredArr.sort((a, b) => a - b);
+  //   console.log(filteredArr);
+  // }
+
+  const handleSearch = (input: string) => {
+
+    // const ByName = (input: any) => {
+    //   return (obj: any) => obj.gender === input;
+    // }
+
+    // const result = data.filter(ByName(input));
+    setSearchInput(input);
+    const filteredData = data.filter((item) => {
+      // Convert the item object to a string and check if it contains the search term
+      const itemString = JSON.stringify(item).toLowerCase();
+      return itemString.includes(input.toLowerCase());
+      
+    });
+    setSortedData(filteredData);
+    
+  }
 
   return (
     <div className="">
-      <ItemList />
+      <input onChange={(e) => handleSearch(e.target.value)} placeholder="search"></input>
+      <button>Search</button>
+      <button onClick={sortAlphabetically}>Sort Alphabetically</button>
+      <ItemList sortedData={sortedData}/>
       <div className="d-flex justify-content-center mt-4">
       <Paginate
         previousLabel={"<"}
--- src/Services/DataService.tsx
+++ src/Services/DataService.tsx
@@ -1,10 +1,48 @@
 
 let userData = {};
 
+interface PeopleData {
+    picture: {
+      medium: string
+    }
+    name: {
+      first: string,
+      last: string
+    }
+    cell: string,
+    email: string,
+    gender: string,
+    location: {
+      country: string
+    }
+  }
+
+  interface PeopleAPIResponse {
+    data: PeopleData[];
+  }
+
 const GetRandomUserData = async () => {
     const response = await fetch('https://randomuser.me/api/?results=50')
-    const data = response.json();
-    return data;
+    const jsonData = await response.json();
+    const parsedJsonData = jsonData.results;
+    const parsedData: PeopleAPIResponse = {
+        data: parsedJsonData.map((userData: any) => ({
+            picture: {
+                medium: userData.picture.medium
+              },
+              name: {
+                first: userData.name.first,
+                last: userData.name.last
+              },
+              cell: userData.cell,
+              email: userData.email,
+              gender: userData.gender,
+              location: {
+                country: userData.location.country
+              }
+        }))
+    }
+    return parsedData;
 }
 
 const createAccount = async (createdUser: any) => {
@@ -49,4 +87,4 @@ const GetLoggedInUserData = async (username: any) => {
     return data;
 }
 
-export { GetRandomUserData, createAccount, login, GetLoggedInUserData };
+export { GetRandomUserData, createAccount, login, GetLoggedInUserData, PeopleAPIResponse, PeopleData };
