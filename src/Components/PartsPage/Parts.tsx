93da7674f721cd63ee982391fa8cac341f202889
nauticuff <felipecarlos200x@gmail.com>
2023-05-17 14:46:56 -0700

    cpu filters working, but needs page reset to 1

--- src/Components/PartsPage/Parts.tsx
+++ src/Components/PartsPage/Parts.tsx
@@ -25,6 +25,8 @@ export default function Parts() {
     const navigate = useNavigate();
     const size = useWindowSize();
 
+    const [currentPage, setCurrentPage] = useState<number>(0);
+
     const [selectedComponent, setSelectedComponent] = useState<string>('');
     const [minBudget, setMinBudget] = useState<number>(0);
     const [maxBudget, setMaxBudget] = useState<number>(0);
@@ -54,44 +56,44 @@ export default function Parts() {
     async function handleComponentSelect(component: string) {
 
         const data = await GetPartData(component);
-        if (component == "Cpu") {
+        if (component === "Cpu") {
             setCpuData(data);
             setTotalItems(data.length)
             setComponentType('Cpu')
-        } else if (component == "Gpu") {
+        } else if (component === "Gpu") {
             setGpuData(data);
             setTotalItems(data.length)
             setComponentType('Gpu')
-        } else if (component == "Motherboard") {
+        } else if (component === "Motherboard") {
             setMotherboardData(data);
             setTotalItems(data.length)
             setComponentType('Motherboard')
-        } else if (component == "Case") {
+        } else if (component === "Case") {
             setCaseData(data)
             setTotalItems(data.length)
             setComponentType('Case')
-        } else if (component == "Ram") {
+        } else if (component === "Ram") {
             setRamData(data)
             setTotalItems(data.length)
             setComponentType('Ram')
-        } else if (component == 'Ps') {
+        } else if (component === 'Ps') {
             setPsData(data)
             setTotalItems(data.length)
             setComponentType('Ps')
-        } else if (component == "Heatsink") {
+        } else if (component === "Heatsink") {
             setHeatsinkData(data)
             setTotalItems(data.length)
             setComponentType('Heatsink')
-        } else if (component == "HardDrive") {
+        } else if (component === "HardDrive") {
             setTotalItems(data.length)
             setHardDriveData(data)
             setComponentType('HardDrive')
         }
-        setCpuManufacturers([{
-            All: true,
-            AMD: false,
-            Intel: false
-        }])
+        // setCpuManufacturers([{
+        //     All: true,
+        //     AMD: false,
+        //     Intel: false
+        // }])
 
     }
 
@@ -102,10 +104,9 @@ export default function Parts() {
             setCpuData(data);
             setTotalItems(data.length)
             setComponentType('Cpu')
-            console.log(data)
         }
         getData()
-
+        
     }, [])
 
     //HandlesPaginationButtons
@@ -113,18 +114,14 @@ export default function Parts() {
         setCurrentPage(selectedPage.selected);
     };
 
-    const [currentPage, setCurrentPage] = useState<number>(0);
-
     const ITEMS_PER_PAGE = 6;
-
-
 
     const totalPages = Math.ceil(totalItems / ITEMS_PER_PAGE);
 
     const SwitchComponent = () => {
         switch (componentType) {
             case 'Cpu':
-                return <CpuList cpuData={cpuData} currentPage={currentPage} setCurrentPage={setCurrentPage} />;
+                return <CpuList cpuData={cpuData} currentPage={currentPage} setCurrentPage={setCurrentPage}/>;
             case 'Gpu':
                 return <GpuList gpuData={gpuData} currentPage={currentPage} setCurrentPage={setCurrentPage} />;
             case 'Motherboard':
@@ -158,32 +155,31 @@ export default function Parts() {
         setMaxBudget(value);
     };
 
-    const handleCpuManufacturer = () => {
-        if (cpuManufacturers[0]['All'] === true) {
-            setCpuData(originalCpuData)
-            setTotalItems(originalCpuData.length)
-        } else {
-            let data: any = [];
+    // const handleCpuManufacturer = () => {
+    //     if (cpuManufacturers[0]['All'] === true) {
+    //         setCpuData(originalCpuData)
+    //         setTotalItems(originalCpuData.length)
+    //     } else {
+    //         let data: any = [];
 
-            const filteredData = cpuData.filter((cpu: { title: string, index?: number }) => {
-                let included = false;
-                Object.keys(cpuManufacturers[0]).forEach(key => {
-                    if (cpu.title.includes(key) && cpuManufacturers[0][key as keyof typeof cpuManufacturers[0]]) {
-                        included = true;
-                    }
-                });
-                return included;
-            }).map((cpu) => {
-                //cpu.index = index;
-                return cpu;
-            });
+    //         const filteredData = cpuData.filter((cpu: { title: string, index?: number }) => {
+    //             let included = false;
+    //             Object.keys(cpuManufacturers[0]).forEach(key => {
+    //                 if (cpu.title.includes(key) && cpuManufacturers[0][key as keyof typeof cpuManufacturers[0]]) {
+    //                     included = true;
+    //                 }
+    //             });
+    //             return included;
+    //         }).map((cpu) => {
+    //             return cpu;
+    //         });
 
-            data = [...data, filteredData]
+    //         data = [...data, filteredData]
 
-            setCpuData(filteredData);
-            setTotalItems(filteredData.length)
-        }
-    }
+    //         setCpuData(filteredData);
+    //         setTotalItems(filteredData.length)
+    //     }
+    // }
 
     const handleCpuFiltersCheckbox = () => {
         if (cpuFilters.manufacturers.All && cpuFilters.socketTypes.All) {
@@ -204,7 +200,6 @@ export default function Parts() {
 
                 return included;
             }).map((cpu) => {
-                //cpu.index = index;
                 return cpu;
             });
 
@@ -227,7 +222,6 @@ export default function Parts() {
 
                 return included;
             }).map((cpu) => {
-                //cpu.index = index;
                 return cpu;
             });
 
@@ -238,33 +232,9 @@ export default function Parts() {
 
         
         }
-    //DEBUG THISSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS 
-    //DEBUG THISSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS 
-    //DEBUG THISSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS 
-    //DEBUG THISSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS 
-    //DEBUG THISSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS 
-    //DEBUG THISSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS 
-    //DEBUG THISSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS 
-    //DEBUG THISSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS 
-        else {
-            let data: any[] = [];
-            const filteredData = originalCpuData.filter((cpu: { socketType: string, index?: number }) => {
-                let included = false;
-                Object.keys(cpuSocket).forEach((key) => {
-                    if (cpu.socketType.includes(key) && cpuSocket[key as keyof typeof cpuSocket]) {
-                        included = true;
-                    }
-                });
-                return included;
-            }).map((cpu) => {
-                //cpu.index = index;
-                return cpu;
-            });
-
-            data = [...data, filteredData];
-
-            let data3: any[] = [];
-            const data2 = filteredData.filter((cpu: { title: string, index?: number }) => {
+   
+        else if(!cpuFilters.manufacturers.All && !cpuFilters.socketTypes.All){
+            let data = originalCpuData.filter((cpu: { title: string, index?: number }) => {
                 let included = false;
                 const { manufacturers } = cpuFilters;
 
@@ -276,58 +246,72 @@ export default function Parts() {
 
                 return included;
             }).map((cpu) => {
-                //cpu.index = index;
                 return cpu;
             });
 
-            data3 = [...data3, data2]
-
-            setCpuData(data2);
-            setTotalItems(data2.length);
-        }
-    }
-
-    const handleCpuSocketType = () => {
-        if (cpuSocket.All === true) {
-            setCpuData(originalCpuData);
-            setTotalItems(originalCpuData.length);
-        } else {
-            let data: any[] = [];
-
-            const filteredData = cpuData.filter((cpu: { socketType: string, index?: number }) => {
+            data = data.filter((cpu: { socketType: string, index?: number }) => {
                 let included = false;
-                Object.keys(cpuSocket).forEach((key) => {
-                    if (cpu.socketType.includes(key) && cpuSocket[key as keyof typeof cpuSocket]) {
+                const { socketTypes } = cpuFilters;
+
+                Object.keys(socketTypes).forEach((key) => {
+                    if (cpu.socketType.includes(key) && socketTypes[key as keyof typeof socketTypes]) {
                         included = true;
                     }
                 });
+
                 return included;
             }).map((cpu) => {
-                //cpu.index = index;
                 return cpu;
             });
 
-            data = [...data, filteredData];
-
-            setCpuData(filteredData);
-            setTotalItems(filteredData.length);
+            setCpuData(data);
+            setTotalItems(data.length);
+            console.log(data)
         }
+        setCurrentPage(1)
     }
 
+    // const handleCpuSocketType = () => {
+    //     if (cpuSocket.All === true) {
+    //         setCpuData(originalCpuData);
+    //         setTotalItems(originalCpuData.length);
+    //     } else {
+    //         let data: any[] = [];
+
+    //         const filteredData = cpuData.filter((cpu: { socketType: string, index?: number }) => {
+    //             let included = false;
+    //             Object.keys(cpuSocket).forEach((key) => {
+    //                 if (cpu.socketType.includes(key) && cpuSocket[key as keyof typeof cpuSocket]) {
+    //                     included = true;
+    //                 }
+    //             });
+    //             return included;
+    //         }).map((cpu) => {
+    //             //cpu.index = index;
+    //             return cpu;
+    //         });
+
+    //         data = [...data, filteredData];
+
+    //         setCpuData(filteredData);
+    //         setTotalItems(filteredData.length);
+    //     }
+    // }
+
     useEffect(() => {
-        if (componentType == 'Cpu') {
+        if (componentType === 'Cpu') {
             handleCpuFiltersCheckbox()
-        } else if (componentType == 'Gpu') {
+        } else if (componentType === 'Gpu') {
             setGpuData(sortByPrice(gpuData));
-        } else if (componentType == "Motherboard") {
+        } else if (componentType === "Motherboard") {
             setMotherboardData(sortByPrice(motherboardData));
-        } else if (componentType == "Case") {
+        } else if (componentType === "Case") {
             setCaseData(sortByPrice(caseData));
-        } else if (componentType == "Ram") {
+        } else if (componentType === "Ram") {
             setRamData(sortByPrice(ramData));
-        } else if (componentType == "Ps") {
+        } else if (componentType === "Ps") {
             setPsData(sortByPrice(psData));
-        } else if (componentType == "Heatsink") {
+        } else if (componentType === "Heatsink") {
             setHeatsinkData(sortByPrice(heatsinkData));
         } else {
             setHardDriveData(sortByPrice(hardDriveData));
@@ -769,15 +753,15 @@ export default function Parts() {
                                 LGA2011
                             </label>
                         </div>
-                        <InputGroup className="mb-3 flex align-items-center gap-2">
+                        {/* <InputGroup className="mb-3 flex align-items-center gap-2">
                             <InputGroup.Checkbox />AMD
                         </InputGroup>
                         <InputGroup className="mb-3 flex align-items-center gap-2">
                             <InputGroup.Checkbox />Intel
-                        </InputGroup>
+                        </InputGroup> */}
                         {/*Rgb or No Rgb  */}
                         {/* ========================================================== */}
-                        <Button onClick={() => priceSort()}>Apply filters</Button>
+                        {/* <Button onClick={() => priceSort()}>Apply filters</Button> */}
                     </div>
                 </Col>
                 <Col md={9} className='px-2'>
